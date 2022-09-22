e1c644622906ae4b76fcf817921989e5cf8f2679
772178+iamjustsam@users.noreply.github.com <772178+iamjustsam@users.noreply.github.com>
2022-09-23 01:34:33 +0200

    Fixes

--- src/clients/pages/ClientsPage/ClientsPage.tsx
+++ src/clients/pages/ClientsPage/ClientsPage.tsx
@@ -1,36 +1,25 @@
 import React from 'react';
 import {
-    Switch,
-    Route,
-    useRouteMatch,
-    match,
-    useHistory,
+    Route, Routes, useNavigate
 } from "react-router-dom";
 import { Page } from '../../../components';
 import { ClientsTable } from '../../containers/ClientsTable/ClientsTable';
 
 export function ClientsPage() {
-    const { url, path }: match = useRouteMatch();
-    const { push } = useHistory();
-    const gotToCreateClient = () => push(`${url}/create`);
+    const navigate = useNavigate ();
+    const gotToCreateClient = () => navigate('./create');
 
     return (
-        <Switch>
-            <Route exact path={path}>
-                <Page title="Clients" onCreateButtonClick={gotToCreateClient}>
+        <Routes>
+            <Route path="/" element={<Page title="Clients" onCreateButtonClick={gotToCreateClient}>
                     <ClientsTable></ClientsTable>
-                </Page>
-            </Route>
-            <Route path={`${path}/create`}>
-                <Page title="Create client">
+                </Page>} />
+            <Route path="./create" element={<Page title="Create client">
                     Here will come the CreateClient container
-                </Page>
-            </Route>
-            <Route path={`${path}/:clientId/edit`}>
-                <Page title="Edit client">
+                </Page>} />
+            <Route path=":clientId/edit" element={<Page title="Edit client">
                     Here will come the EditClient container
-                </Page>
-            </Route>
-        </Switch>
+                </Page>} />
+        </Routes>
     );
 }
