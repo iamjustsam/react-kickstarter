52810ce409dcaf45a978ad2b714e085cbe17d3f5
772178+iamjustsam@users.noreply.github.com <772178+iamjustsam@users.noreply.github.com>
2022-09-22 23:30:16 +0200

    ch03

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import { Header } from "./components";
+import React, { useState } from "react";
+import { Sidenav, SidenavContainer, SidenavContent, Header } from './components';
 
 export function App() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
     return (
-        <div className="w-full h-full">
-            <Header
-                title="Hello, jworks!"
-                handleClickMenuButton={() => console.log('the menu button was clicked')}
-            ></Header>
+        <div className="w-full h-full flex flex-col">
+            <Header title='Jworks base app' handleClickMenuButton={() => setIsSidebarOpen(!isSidebarOpen)} />
+            <div className="flex flex-1 flex-auto">
+                <SidenavContainer>
+                    <Sidenav isSidenavOpen={isSidebarOpen}>
+                        <div>Item 1</div>
+                        <div>Item 1</div>
+                    </Sidenav>
+                    <SidenavContent>
+                    </SidenavContent>
+                </SidenavContainer>
+            </div>
         </div>
     );
 }
