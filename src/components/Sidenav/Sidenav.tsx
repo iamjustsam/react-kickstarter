b0b68517ddde5c5c4c3b83e9dbaf59769776931c
772178+iamjustsam@users.noreply.github.com <772178+iamjustsam@users.noreply.github.com>
2022-09-23 00:41:04 +0200

    Fixes SideNav

--- src/components/Sidenav/Sidenav.tsx
+++ src/components/Sidenav/Sidenav.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { useTransition, animated } from "react-spring";
+import { animated, useTransition } from "react-spring";
 
-export const SidenavContainer: React.FC = React.memo(({ children }) => (
+interface SidenavContainerProps {
+  children: React.ReactNode;
+}
+
+export const SidenavContainer: React.FC<SidenavContainerProps> = React.memo(({ children }) => (
   <div
     data-testid="sidenav-container"
     className="flex flex-row flex-1 overflow-hidden h-full"
@@ -12,39 +16,40 @@ export const SidenavContainer: React.FC = React.memo(({ children }) => (
 
 interface SidenavProps {
   isSidenavOpen: boolean;
+  children: React.ReactNode;
 }
 
 export const Sidenav: React.FC<SidenavProps> = React.memo(
   ({ children, isSidenavOpen }) => {
     const width = "14rem";
-    const transitions = useTransition(isSidenavOpen, null, {
+    const transitions = useTransition(isSidenavOpen, {
       from: { marginLeft: `-${width}` },
       enter: { marginLeft: "0" },
       leave: { marginLeft: `-${width}` },
     });
     return (
       <>
-        {transitions.map(
-          ({ item, key, props }) =>
-            item && (
-              <animated.div key={key} style={props}>
-                <div
-                  data-testid="sidenav"
-                  style={{ width }}
-                  className={`p-6 border-r border-black flex-grow-0 flex-shrink-0 h-full`}
-                >
-                  {children}
-                </div>
-                ️
-              </animated.div>
-            )
-        )}
+        {
+          transitions((styles, item) => item && <animated.div style={styles}>
+            <div
+              data-testid="sidenav"
+              style={{ width }}
+              className={`p-6 border-r border-black flex-grow-0 flex-shrink-0 h-full`}
+            >
+              {children}
+            </div>
+          </animated.div>)
+        }
       </>
     );
   }
 );
 
-export const SidenavContent: React.FC = React.memo(({ children }) => (
+interface SideNavContentProps {
+  children: React.ReactNode;
+}
+
+export const SidenavContent: React.FC<SideNavContentProps> = React.memo(({ children }) => (
   <div className="flex-1 p-2" data-testid="sidenav-content">
     {children}
   </div>
