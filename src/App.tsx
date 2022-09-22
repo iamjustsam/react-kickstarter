1153c229c252e4775dc7b9461d48a59e7e1090a3
772178+iamjustsam@users.noreply.github.com <772178+iamjustsam@users.noreply.github.com>
2022-09-22 23:55:05 +0200

    ch05

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Sidenav, SidenavContainer, SidenavContent, Header, NavItems } from './components';
 import { Switch, Route } from "react-router-dom";
 import { Routes } from './routes';
+import { ClientsPage } from './clients';
 
 const navItems = [{
     name: 'products',
@@ -17,21 +18,21 @@ export function App() {
         <div className="w-full h-full flex flex-col">
             <Header title='Jworks base app' handleClickMenuButton={() => setIsSidebarOpen(!isSidebarOpen)}/>
             <div className="flex flex-1 flex-auto">
-            <SidenavContainer>
-                <Sidenav isSidenavOpen={isSidebarOpen}>
-                    <NavItems navItems={navItems}/>
-                </Sidenav>
-                <SidenavContent>
-                    <Switch>
-                        <Route path={Routes.PRODUCTS}>
-                            Here comes the products page
-                        </Route>
-                        <Route path={Routes.CLIENTS}>
-                            Here comes the clients page
-                        </Route>
-                    </Switch>
-                </SidenavContent>
-            </SidenavContainer>
+                <SidenavContainer>
+                    <Sidenav isSidenavOpen={isSidebarOpen}>
+                        <NavItems navItems={navItems}/>
+                    </Sidenav>
+                    <SidenavContent>
+                        <Switch>
+                            <Route path={Routes.PRODUCTS}>
+                                Here comes the products page
+                            </Route>
+                            <Route path={Routes.CLIENTS}>
+                                <ClientsPage/>
+                            </Route>
+                        </Switch>
+                    </SidenavContent>
+                </SidenavContainer>
             </div>
         </div>
     );
